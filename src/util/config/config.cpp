738099f8b31e04ba7d3e12617db4c9e5c0607ed4
Alpyne <alpyne.dreams@gmail.com>
2022-05-14 21:39:48 -0700

    [d3d8] Add forceD16 hack
    Reduces Z-fighting in Duke Nukem Forever (2001).

--- src/util/config/config.cpp
+++ src/util/config/config.cpp
@@ -634,6 +634,18 @@ namespace dxvk {
     { R"(\\(hammer(plusplus)?|mallet|wc)\.exe$)", {{
       { "d3d9.apitraceMode",                "True" },
     }} },
+
+    /**********************************************/
+    /* D3D8 GAMES                                 */
+    /**********************************************/
+
+    /* Duke Nukem Forever (2001)                  *
+     * Hack: force D16 to reduce Z-fighting       *
+     * on shadows.                                */
+    { R"(\\DukeForever\.exe$)", {{
+      { "d3d9.maxFrameRate",                "60"   },
+      { "d3d8.forceD16",                    "True" },
+    }} },
   }};
 
 
